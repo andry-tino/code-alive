a2cce51fbf1f1f5bd6303918605de40b07ea0a47
Andrea <andry.tino@gmail.com>
2018-06-24 00:14:44 +0200

    Adding unity project for wcf example

---
+++ examples/wcf-communication/src/unity-process/WcfCommunication/Assets/SceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
