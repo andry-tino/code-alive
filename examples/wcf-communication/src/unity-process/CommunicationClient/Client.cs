211c3f24fe010b9750d62a5f9a1b8051af23a513
Andrea <andry.tino@gmail.com>
2018-06-26 07:17:20 +0200

    Adding client to wcf example

---
+++ examples/wcf-communication/src/unity-process/CommunicationClient/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace CommunicationClient
+{
+    public class Client
+    {
+        public static void Main()
+        {
+            using (var cf = new ChannelFactory<ICommunicationService>(new WebHttpBinding(), "http://localhost:8000"))
+            {
+                cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
+                ICommunicationService channel = cf.CreateChannel();
+
+                Console.WriteLine("Calling EchoWithGet via HTTP GET: ");
+                string response = channel.EchoWithGet("Hello, world");
+                Console.WriteLine("Output: {0}", response);
+            }
+        }
+    }
+}
---
+++ examples/wcf-communication/src/unity-process/CommunicationClient/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace CommunicationClient
+{
+    [ServiceContract]
+    public interface ICommunicationService
+    {
+        [OperationContract]
+        [WebGet]
+        string EchoWithGet(string s);
+
+        [OperationContract]
+        [WebInvoke]
+        string EchoWithPost(string s);
+    }
+}
