73b41da9d9626693c5df3e5ec5137282c9d5a51d
Andrea <andry.tino@gmail.com>
2018-06-27 23:12:29 +0200

    Creating example project for using NTS Triangulation

---
+++ examples/proc-mesh/src/TriangulationExample/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NTS = NetTopologySuite;
+
+namespace TriangulationExample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
