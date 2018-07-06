ccb90260a68a746a463959094d23371568d9f931
Andrea <andry.tino@gmail.com>
2018-07-07 00:29:32 +0200

    Better separation

--- src/triangulator/src/DelaunayTriangulator/triangulation.cpp
+++ src/triangulator/src/DelaunayTriangulator/triangulation.cpp
@@ -13,6 +13,13 @@
 
 #include "triangulator.h"
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Triangulation_3<K>      Triangulation;
+typedef Triangulation::Cell_handle    Cell_handle;
+typedef Triangulation::Vertex_handle  Vertex_handle;
+typedef Triangulation::Locate_type    Locate_type;
+typedef Triangulation::Point          Point;
+
 int maintri()
 {
   // construction from a list of points :
