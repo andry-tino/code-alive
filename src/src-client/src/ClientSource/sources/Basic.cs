665e7710d3314cfb1e2fa669116834666c59a95d
Andrea <andry.tino@gmail.com>
2018-07-23 17:13:25 +0200

    Complete client source API invocation

--- src/src-client/src/ClientSource/sources/Basic.cs
+++ src/src-client/src/ClientSource/sources/Basic.cs
@@ -22,45 +22,59 @@ namespace CodeAlive.ClientSource.Basic
         /// </summary>
         public void Run()
         {
+            // Create an instance (the root object)
+            this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Create an instance" });
+            this.svc.RenderNewCell(new NewInstanceRenderingRequest() { InstanceId = "root" });
+
             // Create an instance
             var person = new Person();
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Create an instance" });
+            this.svc.RenderNewCell(new NewInstanceRenderingRequest() { InstanceId = "Person@person" });
 
             // Assign a property
             person.Name = "Claus";
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Assign a property" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Person.Name", SourceInstanceId = "root", DstInstanceId = "Person@person" });
 
             // Assign a property
             person.Surname = "Valca";
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Assign a property" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Person.Surname", SourceInstanceId = "root", DstInstanceId = "Person@person" });
 
             // Create an instance
             var piano = new Instrument();
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Create an instance" });
+            this.svc.RenderNewCell(new NewInstanceRenderingRequest() { InstanceId = "Instrument@piano" });
 
             // Assign a property
             piano.Name = "Classic Piano";
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Assign a property" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Instrument.Name", SourceInstanceId = "root", DstInstanceId = "Instrument@piano" });
 
             // Create an instance
             var harmonica = new Instrument();
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Create an instance" });
+            this.svc.RenderNewCell(new NewInstanceRenderingRequest() { InstanceId = "Instrument@harmonica" });
 
             // Assign a property
             harmonica.Name = "Harmonica";
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Assign a property" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Instrument.Name", SourceInstanceId = "root", DstInstanceId = "Instrument@harmonica" });
 
             // Assign a property
             person.Instrument1 = piano;
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Assign a property" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Person.Instrument1", SourceInstanceId = "root", DstInstanceId = "Person@person" });
 
             // Assign a property
             person.Instrument2 = harmonica;
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Assign a property" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Person.Instrument2", SourceInstanceId = "root", DstInstanceId = "Person@person" });
 
             // Execute a method returning a value
             var result = person.Play();
             this.svc.Echo(new DiagnosticRenderingRequest() { Content = "Execute a method returning a value" });
+            this.svc.RenderInteraction(new InteractionRenderingRequest() { InvocationName = "Person.Play", SourceInstanceId = "root", DstInstanceId = "Person@person" });
         }
 
         #region Types
