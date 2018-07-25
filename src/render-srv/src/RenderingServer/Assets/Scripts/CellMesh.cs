c1db3dba1e3cb87f40abc3e431b5dccd4104caae
Andrea <andry.tino@gmail.com>
2018-07-25 03:43:57 +0200

    Better animation for surface change in cells

--- src/render-srv/src/RenderingServer/Assets/Scripts/CellMesh.cs
+++ src/render-srv/src/RenderingServer/Assets/Scripts/CellMesh.cs
@@ -36,6 +36,12 @@ public class CellMesh : MonoBehaviour
 
     #endregion
 
+    private Vector3[] vertices; // Original vertices
+    private System.Random rnd = new System.Random();
+
+    private int updateCounter = 0;
+    private int updateThreshold = 16;
+
     void Start()
     {
         //this.CreateBasicVoxel();
@@ -50,14 +56,14 @@ public class CellMesh : MonoBehaviour
         // Handle vertices
         int numberOfVertices = codealive_triangulator_get_vertices_num();
 
-        Vector3[] vertices = new Vector3[numberOfVertices];
+        this.vertices = new Vector3[numberOfVertices];
         for (int i = 0; i < numberOfVertices; i++)
         {
             double x = codealive_triangulator_get_vertex(i, 0);
             double y = codealive_triangulator_get_vertex(i, 1);
             double z = codealive_triangulator_get_vertex(i, 2);
 
-            vertices[i] = new Vector3((float)x, (float)y, (float)z);
+            this.vertices[i] = new Vector3((float)x, (float)y, (float)z);
         }
 
         // Handle triangles
@@ -77,61 +83,41 @@ public class CellMesh : MonoBehaviour
 
         // Build mesh
         var mesh = GetComponent<MeshFilter>().mesh = new Mesh();
-        mesh.vertices = vertices;
-        mesh.triangles = triangles;
-        mesh.normals = normals;
-    }
-
-    void CreateBasicVoxel()
-    {
-        var vertices = new Vector3[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 2, 0),
-            new Vector3(1, 1, 1),
-            new Vector3(2, 0, 0),
-            new Vector3(2, 2, 0)
-        };
-
-        var triangles = new int[]
-        {
-            4, 1, 2,
-            4, 2, 3,
-            1, 4, 3,
-            1, 0, 2,
-            3, 0, 1,
-            2, 0, 3
-        };
-
-        var normals = new Vector3[]
-        {
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward
-        };
-
-        var mesh = GetComponent<MeshFilter>().mesh = new Mesh();
-        mesh.vertices = vertices;
+        mesh.vertices = this.vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
     }
 
     void Update()
     {
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = mesh.vertices;
-        Vector3[] normals = mesh.normals;
-
-        System.Random rnd = new System.Random();
-
-        for (int i = 0, l = vertices.Length; i < l; i++)
+        this.updateCounter = (this.updateCounter + 1) % this.updateThreshold;
+        if (this.updateCounter != 0)
         {
-            vertices[i] += normals[i] * 0.002f * rnd.Next(0, 10) * Mathf.Sin(Time.time);
+            return;
         }
 
-        mesh.vertices = vertices;
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] newVertices = new Vector3[this.vertices.Length];
+        
+        for (int i = 0, l = vertices.Length; i < l; i++)
+        {
+            float spicex = (float)this.rnd.NextDouble() - 0.5f; // Distributed in [-.5, .5]
+            if (spicex > 0)
+            {
+                // Don't spice this vertex => Skip it
+                newVertices[i] = this.vertices[i];
+
+                continue;
+            }
+
+            float spicey = (float)this.rnd.NextDouble() - 0.5f;
+            float spicez = (float)this.rnd.NextDouble() - 0.5f;
+            Vector3 rndAdditiveVector = 0.35f * new Vector3(spicex, spicey, spicez);
+
+            newVertices[i] = this.vertices[i] + rndAdditiveVector;
+        }
+
+        mesh.vertices = newVertices;
     }
 
     private static int[] ExtractArray(IntPtr ptr, int arrayLength)
