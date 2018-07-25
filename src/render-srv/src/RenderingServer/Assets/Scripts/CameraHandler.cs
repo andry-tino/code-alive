64dd202127d9439a142629427cc754c7e7da5da5
Andrea <andry.tino@gmail.com>
2018-07-25 06:24:29 +0200

    Add camera revolution

---
+++ src/render-srv/src/RenderingServer/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,39 @@
+﻿/// <summary>
+/// Andrea Tino - 2018
+/// </summary>
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    #region Parameters
+
+    /// <summary>
+    /// The point to look at while revolving around.
+    /// </summary>
+    public Vector3 RevolutionPoint = new Vector3(0, 0, 0);
+
+    /// <summary>
+    /// The revolution speed.
+    /// </summary>
+    public float Speed = 10f;
+
+    #endregion
+
+    private Transform cameraObject = null;
+
+    void Start() {
+        // Cache it
+        this.cameraObject = GetComponent<Transform>();
+
+        this.cameraObject.LookAt(this.RevolutionPoint); // Makes the camera look to it
+    }
+	
+	void Update() {
+        this.cameraObject.RotateAround(this.RevolutionPoint, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * this.Speed);
+    }
+}
